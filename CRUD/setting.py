fdd1edc990609cd56b012282a87405fa74990954
Akif Eren Ertuğrul <97176524+Kozzmos@users.noreply.github.com>
2025-04-08 02:25:49 +0300

    Update setting.py
    Just fixed some typos

--- CRUD/setting.py
+++ CRUD/setting.py
@@ -9,9 +9,9 @@ db = conn['CRUD_db']
 collection = db['categories']
 # -----------------------------------------------------------
 # Seed Data / Sample Data
-# Proje ilk çalıştırıldığında veri gtabanı yaratılırken buradaki oluşturulan koleksiyonu ile yani data ile yaratılmasına denir
-# Bize sağladığı avantaj örneğin hemen read operasyonlarını hızlıca yazabiliriz. Bir tane user Yaratılmışsa testşer yapılabilir vs vs
-# !!!!!!! UYARI bu bloktaki kodları tek sefer çalıştırın (BEN ÇALIŞTIRDIM)
+# Proje ilk çalıştırıldığında veri tabanı yaratılırken buradaki oluşturulan koleksiyon ile yani data ile yaratılmasına denir
+# Bize sağladığı avantaj örneğin hemen read operasyonlarını hızlıca yazabiliriz. Bir tane user yaratılmışsa testler yapılabilir vs vs
+# !!!!!!! UYARI bu bloktaki kodları tek sefer çalıştırın
 # categories = [
 #     Category('Boxing Gloves', 'Everlast produce best boxing gloves').__dict__,
 #     Category('Punching Bags', 'Everlast produce best punching bags').__dict__,
