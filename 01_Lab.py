c84fdd518874ab73ae5ba40fa2e43b5231a08929
Akif Eren Ertuğrul <97176524+Kozzmos@users.noreply.github.com>
2025-04-08 02:24:27 +0300

    Update 01_Lab.py
    Just formatted the code for easy reading

--- 01_Lab.py
+++ 01_Lab.py
@@ -6,7 +6,7 @@ import re # Regex Modülü
 
 # Connection String oluşturalım ki APP Site'tan Server Side'a erişebilelim.
 
-conn = MongoClient('mongodb://localhost:27017')
+conn = MongoClient('mongodb://localhost:(Port)')
 
 # Server üzerinden bir veritabanı yaratım
 
@@ -18,28 +18,28 @@ collection = db['products']
 
 # region Insert One Record
 
-# productname= input("Name: ")
-# price = input("Price: ")
-# product = {
-#     'name': productname,
-#     'price': price
-# }
-# result = collection.insert_one(product)
-# print(result)
+productname= input("Name: ")
+price = input("Price: ")
+product = {
+    'name': productname,
+    'price': price
+}
+result = collection.insert_one(product)
+print(result)
 
 # endregion
 
 #region Insert Many Record
 
-# product_list = [
-#     {"_id": 1, "name": "Lenovo X1 Carbon", "price": 84.999},
-#     {"_id": 2, "name": "Macbook Pro M3", "price": 184.999},
-#     {"_id": 3, "name": "Asus Zen", "price": 144.999},
-#     {"_id": 4, "name": "Monster Abra A7", "price": 59.999},
-#     {"_id": 5, "name": "Monster Tulpar", "price": 33.999},
-# ]
-# result = collection.insert_many(product_list)
-# print(result)
+product_list = [
+    {"_id": 1, "name": "Lenovo X1 Carbon", "price": 84.999},
+    {"_id": 2, "name": "Macbook Pro M3", "price": 184.999},
+    {"_id": 3, "name": "Asus Zen", "price": 144.999},
+    {"_id": 4, "name": "Monster Abra A7", "price": 59.999},
+    {"_id": 5, "name": "Monster Tulpar", "price": 33.999},
+]
+result = collection.insert_many(product_list)
+print(result)
 
 #endregion
 
@@ -51,51 +51,51 @@ for each in collection.find():
 
 # endregion
 
-# region Fiyat 100k üstü sırala
-# filter_input = {
-#     "price": {
-#         "$gt": 100.000
-#     }
-# }
-# for i in collection.find(filter_input):
-#     print(i)
+region Fiyat 100k üstü sırala
+filter_input = {
+    "price": {
+        "$gt": 100.000
+    }
+}
+for i in collection.find(filter_input):
+    print(i)
 
 # endregion
 
 # region Fiyat 80k'dan az yada eşit sırala
-# filter_input = {
-#     "price": {
-#         "$lte": 80.000
-#     }
-# }
-# for i in collection.find(filter_input):
-#     print(i)
+filter_input = {
+    "price": {
+        "$lte": 80.000
+    }
+}
+for i in collection.find(filter_input):
+    print(i)
 
 # endregion
 
 # region Fiyat 59.999 olan ürünleri sırala
-# filter_input = {
-#     "price": {
-#         "$eq": 59.999
-#     }
-# }
-# for i in collection.find(filter_input):
-#     print(i)
+filter_input = {
+    "price": {
+        "$eq": 59.999
+    }
+}
+for i in collection.find(filter_input):
+    print(i)
 
 # endregion
 
 # region Fiyat aralığı  üstü sırala
-# query = {
-#     "price": {
-#         "$gte": 10.000,
-#         "$lte": 60.000,
-#     },
-#     "name": {
-#         "$regex": "Monster"
-#     }
-# }
-# for i in collection.find(query):
-#     print(i)
+query = {
+    "price": {
+        "$gte": 10.000,
+        "$lte": 60.000,
+    },
+    "name": {
+        "$regex": "Monster"
+    }
+}
+for i in collection.find(query):
+    print(i)
 
 # endregion
 
@@ -104,26 +104,26 @@ for each in collection.find():
 # * baş harf demek,
 # . ise herhangi bir karakter demek (boşluk hariç)
 # örnekteki Sadece L ile başlayanlar demek
-# pattern = re.compile('^L.*')
-# query = {
-#     'name': {"$regex": pattern}
-# }
-#
-# for item in collection.find(query):
-#     pprint(item)
+pattern = re.compile('^L.*')
+query = {
+    'name': {"$regex": pattern}
+}
+
+for item in collection.find(query):
+    pprint(item)
 # endregion
 
 # region Update
 
-# result = collection.update_one(
-#     {'name': 'Monster Abra'}, #filter
-#     {
-#         '$set': {                  #update
-#             'name': 'Machintosh',
-#             'price': 69.999
-#         }
-#     }
-# )
-#
-# print(f"{result.modified_count} adet kayıt güncellendi")
+result = collection.update_one(
+    {'name': 'Monster Abra'}, #filter
+    {
+        '$set': {                  #update
+            'name': 'Machintosh',
+            'price': 69.999
+        }
+    }
+)
+
+print(f"{result.modified_count} adet kayıt güncellendi")
 # endregion
